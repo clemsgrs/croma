597c2d4c182139bd28c848dbed5f4ab26aa9cc91
clement grisi <clementnet@outlook.com>
2026-03-11 01:05:05 +0100

    Bump version to 1.0.0

--- croma/__init__.py
+++ croma/__init__.py
@@ -6,7 +6,7 @@ from croma.metrics.ri import RobustnessIndex
 RI = RobustnessIndex
 MaRI = MarginAwareRobustnessIndex
 CCMR = CrossConfounderMarginRatio
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 __all__ = [
     "RI",
